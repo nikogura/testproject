ab59d8d0cb9f1f058de97b0d96104159bb2126c0
Nik Ogura <nik.ogura@gmail.com>
2019-12-05 07:01:23 -0800

    removed print statement and renamed test in the hopes of reducing confusion.

--- pkg/testproject/testproject_test.go
+++ pkg/testproject/testproject_test.go
@@ -1,14 +1,10 @@
 package testproject
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestSumpthin(t *testing.T) {
-
-	fmt.Print("Running nonsense test.")
-
+func TestProjectTest(t *testing.T) {
 	assert.True(t, 1 == 1, "Basic law of Identity")
 }
